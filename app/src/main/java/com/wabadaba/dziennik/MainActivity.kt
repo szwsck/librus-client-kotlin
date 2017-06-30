b9c301a58fce052ac0e4c56d1178dbc981fe5429
Robert Wysocki <robert.wysocki@syncron.com>
2017-07-01 00:57:57 +0200

    Introduce Bugsnag
    Change-Id: I83def0d390965f9619c5bca81de478f4a59c8002

--- app/src/main/java/com/wabadaba/dziennik/MainActivity.kt
+++ app/src/main/java/com/wabadaba/dziennik/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.wabadaba.dziennik
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.bugsnag.android.Bugsnag
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Bugsnag.init(this)
+        Bugsnag.notify(RuntimeException("Non-fatal"))
     }
 }
