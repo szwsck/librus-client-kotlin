e8d6ecf3b7b5d2ecdde548636132545e9da403a5
shymmq <szymek.wysocki@gmail.com>
2017-07-05 00:10:19 +0200

    Add AuthInfoRepository tests

--- app/src/main/java/com/wabadaba/dziennik/api/AuthInfoRepository.kt
+++ app/src/main/java/com/wabadaba/dziennik/api/AuthInfoRepository.kt
@@ -1,5 +1,6 @@
 package com.wabadaba.dziennik.api
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.preference.PreferenceManager
 
@@ -9,21 +10,24 @@ class AuthInfoRepository(val login: String, context: Context) {
 
     private val prefKey = "${login}_auth_info"
 
+    @SuppressLint("ApplySharedPref")
     fun saveAuthInfo(authInfo: AuthInfo) {
         val stringValue = Parser.mapper.writeValueAsString(authInfo)
         prefs.edit()
                 .putString(prefKey, stringValue)
-                .apply()
+                .commit()
     }
 
     fun getAuthInfo(): AuthInfo {
-        val stringValue = prefs.getString(prefKey, null) ?: throw IllegalStateException("Authorization info for user $login not found")
+        val stringValue = prefs.getString(prefKey, null)
+                ?: throw IllegalStateException("Authorization info for user $login not found")
         return Parser.parse(stringValue, AuthInfo::class)
     }
 
+    @SuppressLint("ApplySharedPref")
     fun deleteAuthInfo() {
         prefs.edit()
                 .remove(prefKey)
-                .apply()
+                .commit()
     }
 }
