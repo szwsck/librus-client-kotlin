20bea182661465b5bcc8a7813b2d0595817ae230
shymmq <szymek.wysocki@gmail.com>
2017-08-26 16:25:43 +0200

    Fix grade header item subtitle

--- app/src/main/java/com/wabadaba/dziennik/ui/grades/GradeHeaderItem.kt
+++ app/src/main/java/com/wabadaba/dziennik/ui/grades/GradeHeaderItem.kt
@@ -19,8 +19,14 @@ class GradeHeaderItem(val subject: Subject) : AbstractExpandableItem<GradeHeader
     override fun bindView(holder: ViewHolder, payloads: MutableList<Any>?) {
         super.bindView(holder, payloads)
         holder.title.text = subject.name
-        holder.subtitle.text = "${subItems.size} ocen"
+        holder.subtitle.text = getSubtitle(subItems.size)
     }
+
+    private fun getSubtitle(x: Int) = "$x " +
+            if (x == 0) "ocen"
+            else if (x == 1) "ocena"
+            else if (x % 10 in 2..4 && x !in 12..14) "oceny"
+            else "ocen"
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val title: TextView = view.findViewById(R.id.grade_header_item_title)
