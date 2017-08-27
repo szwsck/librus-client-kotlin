c1d52e96c20f84db63dc30cc6f7eef150ad0e9cc
shymmq <szymek.wysocki@gmail.com>
2017-08-27 13:22:29 +0200

    Add DetailsDialogBuilder

---
+++ app/src/main/java/com/wabadaba/dziennik/ui/DetailsDialogBuilder.kt
@@ -0,0 +1,66 @@
+package com.wabadaba.dziennik.ui
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.graphics.Typeface
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.StyleSpan
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import com.afollestad.materialdialogs.MaterialDialog
+import com.wabadaba.dziennik.R
+
+class DetailsDialogBuilder(val activity: Activity) {
+
+    var title: String? = null
+    val fields = mutableListOf<Pair<CharSequence, CharSequence>>()
+
+    fun withTitle(title: String): DetailsDialogBuilder {
+        this.title = title
+        return this
+    }
+
+    fun addStyledField(title: SpannableString?, value: SpannableString?): DetailsDialogBuilder {
+        fields.add(Pair(title ?: "", value ?: ""))
+        return this
+    }
+
+    /**
+     * Automatically uses bold font on value
+     */
+    fun addField(title: String?, value: String?): DetailsDialogBuilder {
+        val boldSpan = StyleSpan(Typeface.BOLD)
+        val styledValue = if (value != null) {
+            val ssb = SpannableStringBuilder(value)
+            ssb.setSpan(boldSpan, 0, value.length, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+            ssb
+        } else ""
+        fields.add(Pair(title ?: "", styledValue))
+        return this
+    }
+
+    fun build(): MaterialDialog {
+        val inflater = LayoutInflater.from(activity)
+        @SuppressLint("InflateParams")
+        val dialogLayout = inflater.inflate(R.layout.dialog_details, null)
+        val rootView = dialogLayout.findViewById<ViewGroup>(R.id.dialog_details_root)
+        for ((index, field) in fields.withIndex()) {
+            val fieldLayout = inflater.inflate(R.layout.detail_field, rootView, false)
+            fieldLayout.findViewById<TextView>(R.id.detail_field_title).text = field.first
+            fieldLayout.findViewById<TextView>(R.id.detail_field_value).text = field.second
+            rootView.addView(fieldLayout)
+            if (index < fields.size - 1) {
+                val dividerView = inflater.inflate(R.layout.dialog_details_divider, rootView, false)
+                rootView.addView(dividerView)
+            }
+        }
+        val builder = MaterialDialog.Builder(activity)
+        if (title != null) builder.title(title!!)
+        builder.positiveText(activity.getString(R.string.close))
+        builder.customView(dialogLayout, true)
+        return builder.build()
+    }
+}
