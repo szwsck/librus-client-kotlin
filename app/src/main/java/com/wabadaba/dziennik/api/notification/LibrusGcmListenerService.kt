68287e77bcef7661c1d1e6f8194785f4f8cdfd80
shymmq <szymek.wysocki@gmail.com>
2017-08-30 11:35:36 +0200

    Improve notification behavior

--- app/src/main/java/com/wabadaba/dziennik/api/notification/LibrusGcmListenerService.kt
+++ app/src/main/java/com/wabadaba/dziennik/api/notification/LibrusGcmListenerService.kt
@@ -1,11 +1,15 @@
 package com.wabadaba.dziennik.api.notification
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.NotificationCompat
 import com.google.android.gms.gcm.GcmListenerService
 import com.wabadaba.dziennik.R
+import com.wabadaba.dziennik.ui.MainActivity
+
 
 @Suppress("DEPRECATION")
 class LibrusGcmListenerService : GcmListenerService() {
@@ -13,12 +17,25 @@ class LibrusGcmListenerService : GcmListenerService() {
         println("Received GCM message")
         val message = data.getString("message")
         val user = data.getString("userId")
+
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
         val notification = NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.ic_school_black_24dp)
                 .setContentTitle(message)
                 .setContentText(user)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .build()
+
         val notifyManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notifyManager.notify(1, notification)
+        notifyManager.notify(System.currentTimeMillis().toInt(), notification)
     }
 }
