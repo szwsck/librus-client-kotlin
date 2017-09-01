f083472e1f7345084f0744fe35ee8681a401c6ca
shymmq <szymek.wysocki@gmail.com>
2017-09-01 16:37:33 +0200

    Add about activity, closes #10

---
+++ app/src/main/java/com/wabadaba/dziennik/ui/AboutActivity.kt
@@ -0,0 +1,24 @@
+package com.wabadaba.dziennik.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.wabadaba.dziennik.BuildConfig
+import com.wabadaba.dziennik.R
+import kotlinx.android.synthetic.main.acitivity_about.*
+
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.acitivity_about)
+
+        val manager = packageManager
+        val info = manager.getPackageInfo(
+                packageName, 0)
+        val version = info.versionName
+
+        val flavor = BuildConfig.FLAVOR
+
+        activity_about_version.text = "wersja $version | $flavor"
+    }
+}
