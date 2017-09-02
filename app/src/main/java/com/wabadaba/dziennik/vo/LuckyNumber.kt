6030f3c0ac073a1e139f8f8c355a355bc075a353
Adam Żbikowski <thehdplayerpl@gmail.com>
2017-09-02 20:31:05 +0200

    Add LuckyNumber interface

---
+++ app/src/main/java/com/wabadaba/dziennik/vo/LuckyNumber.kt
@@ -0,0 +1,23 @@
+package com.wabadaba.dziennik.vo
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import io.requery.Entity
+import io.requery.Key
+import org.joda.time.LocalDate
+
+@LibrusEntity("LuckyNumbers")
+@Entity
+@JsonDeserialize(`as` = LuckyNumberEntity::class)
+interface LuckyNumber : Identifiable {
+
+    @get:JsonProperty("LuckyNumber")
+    val number: Int?
+
+    @get:JsonProperty("LuckyNumberDay")
+    val date: LocalDate?
+
+    override val id: String
+        @get:Key
+        get() = this.date.toString()
+}
